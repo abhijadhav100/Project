176e0bbb49a4296e33f60e52a1c934bb5436c5a4
abhijadhav100 <111724525+abhijadhav100@users.noreply.github.com>
2023-01-31 22:04:33 +0530

    File uploaded...

---
+++ script.js
@@ -0,0 +1,9 @@
+function logodir()
+{
+ window.location.href='index.html';
+}false;
+
+/*const logo = document.querySelector('#logo');
+logo = addEventListener("click",function(e){
+ window.location.href='http://www.amazon.com/';
+},false);*/
